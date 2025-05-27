329004c82a2f4440975c215f67f82b0ca0b27baa
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-05-27 12:48:13 +0300

    counter

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import "./styles.css";
+import "@merzin/element";
 
 const app = document.getElementById("app")!;
+
+let counter = 0;
+const button = createElement("button", {
+  innerText: `Counter: ${counter}`,
+  onclick: () => (button.innerText = `Counter: ${++counter}`),
+});
+
+app.append(button);
