626d301e03e21e0b374a658123d1014a9e953c69
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-05 10:53:19 +0300

    http plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app
                 .get_webview_window("main")
