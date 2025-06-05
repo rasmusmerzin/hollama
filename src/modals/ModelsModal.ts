461a11d236191a1ed2d2eeabe06cc0c84f37aa50
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-05 18:36:49 +0300

    models subject

--- src/modals/ModelsModal.ts
+++ src/modals/ModelsModal.ts
@@ -1,67 +1,82 @@
-import "./ModelsModal.css";
 import { ModalWindowBodyElement } from "../elements/ModalWindowBodyElement";
 import { ModalWindowElement } from "../elements/ModalWindowElement";
 import { ModalWindowEntryElement } from "../elements/ModalWindowEntryElement";
 import { ModalWindowHeaderElement } from "../elements/ModalWindowHeaderElement";
 import { ModalWindowTitleBarElement } from "../elements/ModalWindowTitleBarElement";
-import { getAvailableModels, getInstalledModels } from "../services/ollama";
+import { TagElement } from "../elements/TagElement";
+import { Model, ModelsSubject } from "../subjects/ModelsSubject";
 
 export function ModelsModal() {
   let bodyElement: ModalWindowBodyElement;
+  let control: AbortController | undefined;
 
-  return createElement(ModalWindowElement, { className: "models", onconnect }, [
-    createElement(ModalWindowTitleBarElement, { label: "Models" }),
-    (bodyElement = createElement(ModalWindowBodyElement)),
-  ]);
+  return createElement(
+    ModalWindowElement,
+    { className: "models", onconnect, ondisconnect, height: 1080 },
+    [
+      createElement(ModalWindowTitleBarElement, { label: "Models" }),
+      (bodyElement = createElement(ModalWindowBodyElement)),
+    ],
+  );
 
   function onconnect() {
-    renderModels();
+    control?.abort();
+    control = new AbortController();
+    ModelsSubject.subscribe(renderModels, control);
   }
 
-  async function renderModels() {
-    try {
-      const [installed, available] = await Promise.all([
-        getInstalledModels(),
-        getAvailableModels(),
-      ]);
-      if (installed.length)
-        bodyElement.niche.replaceChildren(
-          createElement(ModalWindowHeaderElement, {
-            label: "Installed",
-          }),
-          ...installed.map((model, i) =>
-            createElement(ModalWindowEntryElement, {
-              label: model.name,
-              join: [
-                ...(i > 0 ? ["top"] : []),
-                ...(i < installed.length - 1 ? ["bottom"] : []),
-              ].join(" "),
-            }),
-          ),
-          createElement(ModalWindowHeaderElement, {
-            label: "Available",
-          }),
-          ...available.map((model, i) =>
-            createElement(ModalWindowEntryElement, {
-              label: model.name,
-              join: [
-                ...(i > 0 ? ["top"] : []),
-                ...(i < available.length - 1 ? ["bottom"] : []),
-              ].join(" "),
-            }),
-          ),
-        );
-      else
-        bodyElement.niche.replaceChildren(
-          createElement("div", { className: "info" }, "No models installed."),
-        );
-    } catch (error) {
-      bodyElement.niche.replaceChildren(
-        createElement("div", { className: "info" }, [
-          createElement("p", {}, "Failed to load models."),
-          createElement("p", {}, "Check your Ollama server connection."),
-        ]),
-      );
-    }
+  function ondisconnect() {
+    control?.abort();
+    control = undefined;
+  }
+
+  async function renderModels(models: Record<string, Model>) {
+    const installed = Object.values(models).filter(
+      (model) => model.installed?.length,
+    );
+    const available = Object.values(models).filter(
+      (model) => !model.installed?.length,
+    );
+    bodyElement.niche.replaceChildren(
+      createElement(ModalWindowHeaderElement, {
+        label: "Installed",
+      }),
+      ...constructModelEntries(installed),
+      createElement(ModalWindowHeaderElement, {
+        label: "Available",
+      }),
+      ...constructModelEntries(available),
+    );
+  }
+
+  function constructModelEntries(models: Model[]) {
+    return models.map((model, i) =>
+      createElement(ModalWindowEntryElement, {
+        label: createElement(
+          "div",
+          { style: "display: flex; align-items: center; gap: 4px;" },
+          [
+            createElement(
+              "span",
+              { className: "name", style: "margin-right: 4px" },
+              model.name,
+            ),
+            ...model.tags.map((tag) =>
+              createElement(
+                TagElement,
+                { disabled: !model.installed?.includes(tag) },
+                tag,
+              ),
+            ),
+          ],
+        ),
+        description: model.description,
+        height: 64,
+        join: [
+          ...(i > 0 ? ["top"] : []),
+          ...(i < models.length - 1 ? ["bottom"] : []),
+        ].join(" "),
+      }),
+    );
   }
 }
--- src/services/ollama.ts
+++ src/services/ollama.ts
@@ -1,29 +1,11 @@
 import { fetch } from "@tauri-apps/plugin-http";
-
-export interface InstalledModel {
-  name: string;
-  model: string;
-  modified_at: string;
-  size: number; // in bytes
-  digest: string;
-  details: {
-    parent_model: string;
-    format: string;
-    family: string;
-    families: string[];
-    parameter_size: string;
-    quantization_level: string;
-  };
-}
-
-export interface AvailableModel {
-  name: string;
-}
+import { Model, ModelInstance } from "../subjects/ModelsSubject";
 
 const origin = "http://localhost:11434";
 const hubOrigin = "https://ollama.com";
+const domParser = new DOMParser();
 
-export async function getInstalledModels(): Promise<InstalledModel[]> {
+export async function getInstalledModels(): Promise<ModelInstance[]> {
   const url = new URL("/api/tags", origin);
   const response = await fetch(url);
   if (!response.ok) throw new Error("Failed to fetch installed models");
@@ -31,18 +13,61 @@ export async function getInstalledModels(): Promise<InstalledModel[]> {
   return data.models;
 }
 
-export async function getAvailableModels(): Promise<AvailableModel[]> {
+export async function getAvailableModels(): Promise<Model[]> {
   const url = new URL("/search", hubOrigin);
   const response = await fetch(url);
   if (!response.ok) throw new Error("Failed to fetch available models");
   const html = await response.text();
-  const doc = new DOMParser().parseFromString(html, "text/html");
+  const doc = domParser.parseFromString(html, "text/html");
+  doc.querySelectorAll("script").forEach((s) => s.remove());
   const results = doc.getElementById("searchresults");
   const listItems = results?.querySelectorAll("li") || [];
   return Array.from(listItems, (item) => {
     const anchor = item.querySelector("a");
+    const descriptionElement = item.querySelector("p");
+    const capabilityElements = item.querySelectorAll("[x-test-capability]");
+    const sizeElements = item.querySelectorAll("[x-test-size]");
     const name =
       anchor?.href?.substring(anchor.href.lastIndexOf("/") + 1) || "";
-    return { name };
+    const description = descriptionElement?.innerText.trim() || "";
+    const tags = Array.from(
+      sizeElements,
+      (e) => e.textContent?.trim() || "",
+    ).filter(Boolean);
+    const categories = Array.from(
+      capabilityElements,
+      (e) => e.textContent?.trim() || "",
+    ).filter(Boolean);
+    return { name, description, tags, categories };
   });
+}
+
+export async function getModelDetails(model: string): Promise<Model> {
+  const url = new URL(`/library/${model}`, hubOrigin);
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Failed to fetch model details");
+  const html = await response.text();
+  const doc = domParser.parseFromString(html, "text/html");
+  doc.querySelectorAll("script").forEach((s) => s.remove());
+  const name =
+    doc.querySelector("[x-test-model-name]")?.textContent?.trim() || "";
+  const summaryElement = doc.getElementById("summary");
+  const description = summaryElement?.textContent?.trim() || "";
+  const categories = Array.from(
+    summaryElement?.nextElementSibling?.children || [],
+  )
+    .filter((e) => !e.hasAttribute("x-test-size"))
+    .map((e) => e.textContent?.trim() || "")
+    .filter(Boolean);
+  const anchors = doc.querySelectorAll("a");
+  let latestTag: string | undefined;
+  const tags = Array.from(anchors, (a) => {
+    const [_name, tag] = a.href
+      .substring(a.href.lastIndexOf("/") + 1)
+      .split(":");
+    if (a.nextElementSibling?.textContent?.trim().toLowerCase() === "latest")
+      latestTag = tag;
+    return tag;
+  });
+  return { name, description, tags, latestTag, categories };
 }
