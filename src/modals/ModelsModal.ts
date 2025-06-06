cdab2c14b13ae0edbe2c395fdce3562a9888a8be
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-06 12:32:21 +0300

    model search

--- src/modals/ModelsModal.ts
+++ src/modals/ModelsModal.ts
@@ -1,20 +1,26 @@
+import "./ModelsModal.css";
 import { ModalWindowBodyElement } from "../elements/ModalWindowBodyElement";
 import { ModalWindowElement } from "../elements/ModalWindowElement";
 import { ModalWindowEntryElement } from "../elements/ModalWindowEntryElement";
 import { ModalWindowHeaderElement } from "../elements/ModalWindowHeaderElement";
 import { ModalWindowTitleBarElement } from "../elements/ModalWindowTitleBarElement";
-import { TagElement } from "../elements/TagElement";
 import { Model, ModelsSubject } from "../subjects/ModelsSubject";
+import { TagElement } from "../elements/TagElement";
 
 export function ModelsModal() {
   let bodyElement: ModalWindowBodyElement;
+  let titleBar: ModalWindowTitleBarElement;
   let control: AbortController | undefined;
 
   return createElement(
     ModalWindowElement,
     { className: "models", onconnect, ondisconnect, height: 1080 },
     [
-      createElement(ModalWindowTitleBarElement, { label: "Models" }),
+      (titleBar = createElement(ModalWindowTitleBarElement, {
+        label: "Models",
+        searchable: true,
+        onsearch: renderModels,
+      })),
       (bodyElement = createElement(ModalWindowBodyElement)),
     ],
   );
@@ -30,23 +36,41 @@ export function ModelsModal() {
     control = undefined;
   }
 
-  async function renderModels(models: Record<string, Model>) {
-    const installed = Object.values(models).filter(
-      (model) => model.installed?.length,
-    );
-    const available = Object.values(models).filter(
-      (model) => !model.installed?.length,
-    );
-    bodyElement.niche.replaceChildren(
-      createElement(ModalWindowHeaderElement, {
-        label: "Installed",
-      }),
-      ...constructModelEntries(installed),
-      createElement(ModalWindowHeaderElement, {
-        label: "Available",
-      }),
-      ...constructModelEntries(available),
-    );
+  async function renderModels() {
+    let models = Object.values(ModelsSubject.current());
+    const search = (titleBar.search || "").split(" ").filter(Boolean);
+    models = models.filter((model) => {
+      for (const term of search) {
+        if (
+          !model.name.toLowerCase().includes(term.toLowerCase()) &&
+          !model.description.toLowerCase().includes(term.toLowerCase())
+        ) {
+          return false;
+        }
+      }
+      return true;
+    });
+    const installed = models.filter((model) => model.installed?.length);
+    const available = models.filter((model) => !model.installed?.length);
+    if (installed.length || available.length)
+      bodyElement.niche.replaceChildren(
+        ...(installed.length
+          ? [
+              createElement(ModalWindowHeaderElement, { label: "Installed" }),
+              ...constructModelEntries(installed),
+            ]
+          : []),
+        ...(available.length
+          ? [
+              createElement(ModalWindowHeaderElement, { label: "Available" }),
+              ...constructModelEntries(available),
+            ]
+          : []),
+      );
+    else
+      bodyElement.niche.replaceChildren(
+        createElement("div", { className: "info" }, "No models found."),
+      );
   }
 
   function constructModelEntries(models: Model[]) {
