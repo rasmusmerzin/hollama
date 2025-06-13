d27fe7306b184dff63b4baee646b56f29ea5ee56
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-06-13 19:50:31 +0300

    persistent think toggle

--- src/state/MessageInputSubject.ts
+++ src/state/MessageInputSubject.ts
@@ -1,5 +1,23 @@
 import { Subject } from "./Subject";
 
-export const MessageInputSubject = new Subject({
+export interface MessageInput {
+  think: boolean;
+}
+
+export const MESSAGE_INPUT: MessageInput = Object.freeze({
   think: false,
+});
+
+export const MessageInputSubject = new Subject<MessageInput>(
+  (() => {
+    const stored = localStorage.getItem("message_input");
+    if (stored) return { ...MESSAGE_INPUT, ...JSON.parse(stored) };
+    return { ...MESSAGE_INPUT };
+  })(),
+);
+
+setTimeout(() => {
+  MessageInputSubject.subscribe((messageInput) => {
+    localStorage.setItem("message_input", JSON.stringify(messageInput));
+  }, null);
 });
